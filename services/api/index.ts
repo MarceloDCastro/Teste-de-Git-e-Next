7801aea864dfdeb278c62d860a48dcb7fc3d2e16
Marcelo de Castro <marcelodecastro.ms@gmail.com>
2022-09-08 12:04:37 -0300

    Configuração axios

---
+++ services/api/index.ts
@@ -0,0 +1,16 @@
+
+import axios from 'axios';
+import getConfig from 'next/config';
+
+const baseUrl = `https://novoposdev.advicesystem.com.br/api`;
+// const baseUrl = `https://localhost:7104/api`;
+
+const axiosInstance = axios.create({
+  baseURL: baseUrl,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json;'
+  }
+});
+
+export const api = axiosInstance;
